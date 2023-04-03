f21c28a7d5394f5d2a4e8ce87d9c05bee4e3a30f
Saikat Dutta <saikatdutta25@gmail.com>
2023-04-03 13:27:10 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const App = () => {
+  interface test {
+    name: string;
+    phone: number;
+  }
+  let obj: test[] = [
+    {
+      name: "dhjsfgdhjs",
+      phone: 345646547,
+    },
+  ];
+  return (
+    <>
+      <h1>hello World</h1>
+    </>
+  );
+};
+
+export default App;
